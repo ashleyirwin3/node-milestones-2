b6e32ca55eac38026f48395c2fadc10eaa37a919
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-23 09:57:40 -0500

    added readFile and writeHead to display html content in browser

--- 02_http_server/server.js
+++ 02_http_server/server.js
@@ -1,7 +1,15 @@
 const http = require('http')
+const { readFile } = require('fs')
 const server = http.createServer()
 server.on('request', (req, res) => {
-  console.log('Request received for:', req.url)
-  res.end('OK')
+   readFile('index.html', function (err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length})
+    res.write(data)
+    res.end()
+      console.log('Request received for:', req.url)
+      res.end('OK')
+  })
 })
+
+
 server.listen(8080)
