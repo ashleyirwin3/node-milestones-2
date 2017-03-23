ac8696554cde271b6a0ed42ba94833ce5fc1d4c1
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-23 09:39:21 -0500

    server is built and runs

---
+++ 02_http_server/server.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const server = http.createServer()
+server.on('request', (req, res) => {
+  console.log('Request received for:', req.url)
+  res.end('OK')
+})
+server.listen(8080)
