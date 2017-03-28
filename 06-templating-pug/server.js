81c72848ad99ff0fd83e25a3022783cab7c41866
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-28 13:53:17 -0500

    all views appear in browser

--- 06-templating-pug/server.js
+++ 06-templating-pug/server.js
@@ -8,6 +8,7 @@ app.use(express.static('public'))
 app.set('view engine', 'pug')
 
 const greetings = ["Hello", "Howdy"]
+const menu = ["Carrot Cake", "Orange Creme Pie", "Grapefruit Brownies"]
 app.get('/', (req, res, next) => {
   res.render('index', {subtitle: "Welcome", greetings})
 })
@@ -16,6 +17,10 @@ app.get('/about', (req, res, next) => {
   res.render('about', {subtitle: "Welcome", greetings})
 })
 
+// const menu = ["Carrot Cake", "Orange Creme Pie", "Grapefruit Brownies"]
+app.get('/inventory', (req, res, next) => {
+  res.render('inventory', {subtitle: "Welcome", greetings, menu})
+})
 
 const port = process.env.PORT || 3000
 app.set('port', port)
