0aafb9dbd6bc05de4ca85c6d8ab3ac930fbf6287
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-28 10:52:09 -0500

    live coding SPA with pug

---
+++ pug-livecode/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+
+const names = ["Larry", "Moe", "Curly", "Mr. Rogers"]
+app.get('/', (req, res, next) => {
+  res.render('index', {subtitle: "This came from my JS data", names, loggedIn: false })
+})
+
+app.get('/article', (req, res, next) => {
+  res.render('article', {subtitle: "This came from my JS data", names, loggedIn: true })
+})
+
+const port = process.env.PORT || 3000
+app.set('port', port)
+
+app.listen(port, () =>
+  console.log(`Listening on port ${port}`)
+)
