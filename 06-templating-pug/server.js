e266902986309d6c0bd961c3a12e8597a6ead508
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-28 14:26:31 -0500

    updated content to match instructions

--- 06-templating-pug/server.js
+++ 06-templating-pug/server.js
@@ -7,19 +7,19 @@ app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 
-const greetings = ["Hello", "Howdy"]
-const menu = ["Carrot Cake", "Orange Creme Pie", "Grapefruit Brownies"]
+// const greetings = ["Hello", "Howdy"]
+const menu = ["Carrot Cake-$1,000,000", "Orange Creme Pie-$1,000,000", "Grapefruit Brownies-$1,000,000"]
 app.get('/', (req, res, next) => {
-  res.render('index', {subtitle: "Welcome", greetings})
+  res.render('index', {subtitle: "Welcome"})
 })
 
 app.get('/about', (req, res, next) => {
-  res.render('about', {subtitle: "Welcome", greetings})
+  res.render('about', {subtitle: "Welcome"})
 })
 
 // const menu = ["Carrot Cake", "Orange Creme Pie", "Grapefruit Brownies"]
 app.get('/inventory', (req, res, next) => {
-  res.render('inventory', {subtitle: "Welcome", greetings, menu})
+  res.render('inventory', {subtitle: "Welcome", menu})
 })
 
 const port = process.env.PORT || 3000
