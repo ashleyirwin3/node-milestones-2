5db154592766f43de5c20b09a54750b81c596d10
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-29 13:18:57 -0500

    include nav

--- 06-templating-pug/server.js
+++ 06-templating-pug/server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 
-// const greetings = ["Hello", "Howdy"]
+
 const menu = ["Carrot Cake-$1,000,000", "Orange Creme Pie-$1,000,000", "Grapefruit Brownies-$1,000,000"]
 app.get('/', (req, res, next) => {
   res.render('index', {subtitle: "Welcome"})
@@ -17,9 +17,13 @@ app.get('/about', (req, res, next) => {
   res.render('about', {subtitle: "Welcome"})
 })
 
-// const menu = ["Carrot Cake", "Orange Creme Pie", "Grapefruit Brownies"]
+
 app.get('/inventory', (req, res, next) => {
   res.render('inventory', {subtitle: "Welcome", menu})
+})
+
+app.get('/nav', (req, res, next) => {
+  res.render('nav', {subtitle: "Welcome", menu})
 })
 
 const port = process.env.PORT || 3000
