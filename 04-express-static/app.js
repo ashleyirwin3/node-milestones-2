e76cff9681ef560ac43ac8b27a510703079ba537
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-31 10:50:35 -0500

    required express, built server

---
+++ 04-express-static/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+
+const port = process.env.PORT || 3000
+app.set('port', port)
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
