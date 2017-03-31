84fccce19d0751da40804ac82080af5a264fe5cf
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-03-31 14:45:49 -0500

    deleted files

--- 05-express-middleware/app.js
+++ 05-express-middleware/app.js
@@ -3,10 +3,15 @@
 const express = require('express')
 const app = express()
 
-const routes = require('./routes/')
+const routes = require('../routes/')
 
+const requestTime = (req, res, next) => {
+  req.requestedTime = Date.now()
+  next()
+}
 
-
+app.use(requestTime)
+app.use(routes)
 
 const port = process.env.PORT || 3000
 
